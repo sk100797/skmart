b43244c7a37c3d12f7a6efc7184b0f68c0d2080e
Sai Krishna <saikrishna100797@gmail.com>
2020-06-09 17:25:42 +0530

    added redux thunk

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -5,6 +5,7 @@ import { createStructuredSelector } from "reselect";
 import CollectionPreview from "../collection-preview/collection-preview.component";
 import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
 
+
 import "./collections-overview.styles.scss";
 
 const CollectionsOverview = ({ collections }) => {
